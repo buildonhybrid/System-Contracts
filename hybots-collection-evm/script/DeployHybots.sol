30a5db10e8e00284cd11c5bb00c0f5661914f9c6
danilych <danilych45@proton.me>
2024-07-19 02:21:15 +0200

    feat: update script for deployment

--- hybots-collection-evm/script/DeployHybots.sol
+++ hybots-collection-evm/script/DeployHybots.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-import { Script, console } from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
-import { Hybots } from "src/Hybots.sol";
+import {Hybots} from "src/Hybots.sol";
 
 contract DeployHybots is Script {
     string public baseUri = "";
@@ -13,7 +13,14 @@ contract DeployHybots is Script {
 
         Hybots nft = new Hybots(msg.sender, msg.sender, baseUri);
 
-        console.log("Contract deployed successfully at address: ", address(nft));
+        nft.addRarity("gold");
+        nft.addRarity("bronze");
+        nft.addRarity("silver");
+
+        console.log(
+            "Contract deployed successfully at address: ",
+            address(nft)
+        );
 
         vm.stopBroadcast();
     }
